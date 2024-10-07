ce6022dfe87c3f85a5f78459c466ec40832f6fea
amt-francis-amos <sugartone45@yahoo.com>
2024-10-06 21:07:37 -0800

    update

--- Frontend/src/components/Modals/RoleModal.jsx
+++ Frontend/src/components/Modals/RoleModal.jsx
@@ -37,7 +37,7 @@ const RoleModal = ({ setRole, closeModal }) => {
           </button>
           <button
             onClick={() => handleSelectRole('Admin')}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            className="bg-red-500 text-white px-4 py-2 rounded-md"
           >
             Admin
           </button>
